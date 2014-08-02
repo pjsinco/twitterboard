2d7c74358d65d5044628af0ab54cf87607ef4af4
Patrick Sinco <psinco@gmail.com>
2014-08-02 12:10:18 -0500

    rearranges _master.blade to add persistent left rail nav

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -9,10 +9,51 @@
   <body>
 
     <div class="row">
-      <div class="large-10 columns">
-        @yield('content')
+      <div class="large-12 columns">
+        <h1>TwitterBoard</h1>
+        <hr>
       </div>
-    </div>
+    </div> <!-- end row -->
+
+    <!-- side navigation -->
+    <div class="row">
+      <div class="large-4 columns">
+        <strong>Leaders</strong>
+        <ul class="side-nav">
+          <li><a href="#">Leader tweets</a></li>
+          <li><a href="#">Popular leader tweets</a></li>
+          <li><a href="#">Accounts most mentioned by leaders</a></li>
+          <li><a href="#">Accounts most retweeted by leaders</a></li>
+          <li><a href="#">Tags tweeted by leaders</a></li>
+        </ul>
+        <strong>Circle</strong>
+        <ul class="side-nav">
+          <li><a href="#">All tweets</a></li>
+          <li><a href="#">Search tweets</a></li>
+          <li><a href="#">Search users</a></li>
+          <li><a href="#">Most frequent mentioners</a></li>
+          <li><a href="#">Most frequent retweeters</a></li>
+          <li><a href="#">Tags tweeted by circle</a></li>
+        </ul>
+        <strong>Us</strong>
+        <ul class="side-nav">
+          <li><a href="#">All tweets</a></li>
+          <li><a href="#">Popular tweets</a></li>
+          <li><a href="#">Most frequent mentioners of us</a></li>
+          <li><a href="#">Most frequent retweeters of us</a></li>
+          <li><a href="#">Tags tweeted by us</a></li>
+        </ul>
+      </div> <!-- end large-4 -->
+
+      <div class="large-8 columns">
+        <div class="search">
+          
+        </div>
+        <div class="content">
+          @yield('content')
+        </div>
+      </div> <!-- end large-8 -->
+    </div> <!-- end row -->
 
     <!-- footer -->
     <footer class="row">
