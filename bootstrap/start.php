e1e02dca2574e1268730af9dfc9deb63e9a4ac50
Patrick Sinco <psinco@gmail.com>
2014-08-08 02:07:22 -0500

    mods bootstrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*'),
+    'local' => array('Patricks-MacBook.local'),
+    'production' => array('oneill.asmallorange.com'),
 
 ));
 
